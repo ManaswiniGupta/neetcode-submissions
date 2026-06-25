8e1a49bff5b089254417231752576a1a2fb82f73
Manaswini Gupta <144774277+ManaswiniGupta@users.noreply.github.com>
2026-06-25 17:00:20 +0530

    Add: minimum-size-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        ans=10*5
+        k=0
+        window=[]
+        for i in range(1,len(nums)): 
+            window.append(nums[i])
+            while sum(window)>target:   
+                ans=min(ans,len(window))
+                window.pop(0)
+            
+        if ans==10*5:   
+            return 0    
+        return ans
+        
